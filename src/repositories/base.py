a34e67f1c4bd332a4367ca4d261ec92a5f53a58d
Akylbek Askharov <125436332+AskharovA@users.noreply.github.com>
2024-09-29 14:52:40 +0500

    Edited add method

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,4 +1,5 @@
 from sqlalchemy import select, insert
+from pydantic import BaseModel
 
 
 class BaseRepository:
@@ -17,7 +18,7 @@ class BaseRepository:
         result = await self.session.execute(query)
         return result.scalars().one_or_none()
 
-    async def add(self, **values):
-        add_object_stmt = insert(self.model).values(**values).returning(self.model)
+    async def add(self, data: BaseModel):
+        add_object_stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
         new_object = await self.session.execute(add_object_stmt)
-        return new_object
+        return new_object.scalars().one()
