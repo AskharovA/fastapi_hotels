322bb5aaa2fb4fe7c23f4583a7f4445117ca3963
Akylbek Askharov <125436332+AskharovA@users.noreply.github.com>
2024-09-30 08:58:38 +0500

    Auth Service

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,24 +1,38 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Response
 
 from src.database import async_session_maker
 from src.schemas.users import UserRequestAdd, UserAdd
 from src.repositories.users import UsersRepository
 
-from passlib.context import CryptContext
+from src.services.auth import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и Аутентификация"])
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 @router.post('/register')
 async def register(
         data: UserRequestAdd
 ):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService.hash_password(password=data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
         await UsersRepository(session).add(new_user_data)
         await session.commit()
 
     return {"status": "OK"}
+
+
+@router.post('/login')
+async def login_user(
+        data: UserRequestAdd,
+        response: Response,
+):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail="Пользователь не зарегистрирован")
+        if not AuthService.verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail="Пароль неверный")
+        access_token = AuthService.create_access_token(data={"user_id": user.id})
+        response.set_cookie("access_token", access_token)
+        return {"access_token": access_token}
--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,10 +1,12 @@
 from asyncpg import UniqueViolationError
 from fastapi import HTTPException
+from pydantic import EmailStr
+from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 
 from src.repositories.base import BaseRepository
 from src.models.users import UsersOrm
-from src.schemas.users import User
+from src.schemas.users import User, UserWithHashedPassword
 
 
 class UsersRepository(BaseRepository):
@@ -15,4 +17,10 @@ class UsersRepository(BaseRepository):
         try:
             await super().add(data)
         except (UniqueViolationError, IntegrityError):
-            raise HTTPException(status_code=302, detail="Пользователь уже существует")
+            raise HTTPException(status_code=402, detail="Пользователь уже существует")
+
+    async def get_user_with_hashed_password(self, email: EmailStr):
+        query = select(self.model).filter_by(email=email)
+        result = await self.session.execute(query)
+        model = result.scalars().one()
+        return UserWithHashedPassword.model_validate(model, from_attributes=True)
