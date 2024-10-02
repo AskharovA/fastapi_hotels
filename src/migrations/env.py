1af62e7b00469d6b031e54b74ee828f822416d73
Akylbek Askharov <125436332+AskharovA@users.noreply.github.com>
2024-10-02 10:30:53 +0500

    Task-15 Facilities endpoints

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -12,6 +12,7 @@ from src.models.hotels import HotelsOrm  # noqa
 from src.models.rooms import RoomsOrm  # noqa
 from src.models.users import UsersOrm  # noqa
 from src.models.bookings import BookingsOrm  # noqa
+from src.models.facilities import FacilitiesOrm, RoomsFacilitiesOrm  # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
---
+++ src/migrations/versions/2024_10_02_1029-3218e142dd53_facilities_add.py
@@ -0,0 +1,48 @@
+"""Facilities add
+
+Revision ID: 3218e142dd53
+Revises: d7a211fa836c
+Create Date: 2024-10-02 10:29:18.209435
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "3218e142dd53"
+down_revision: Union[str, None] = "d7a211fa836c"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(length=100), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "rooms_facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("room_id", sa.Integer(), nullable=False),
+        sa.Column("facility_id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["facility_id"],
+            ["facilities.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["room_id"],
+            ["rooms.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("rooms_facilities")
+    op.drop_table("facilities")
