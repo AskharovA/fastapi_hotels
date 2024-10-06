918d8ac0d6e52644ef2a60aa7553a04bf409cbfc
Akylbek Askharov <125436332+AskharovA@users.noreply.github.com>
2024-10-06 17:06:52 +0500

    Facility API Tests update

--- tests/integration_tests/facilities/test_api.py
+++ tests/integration_tests/facilities/test_api.py
@@ -4,8 +4,11 @@ async def test_add_facility(ac):
         json={"title": "Бесплатный Wi-FI"},
     )
     assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+    assert "data" in response.json()
 
 
-async def test_get_facility(ac):
+async def test_get_facilities(ac):
     response = await ac.get("/facilities/")
     assert response.status_code == 200
+    assert isinstance(response.json(), list)
