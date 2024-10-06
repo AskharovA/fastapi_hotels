ac09e590e29c9e219bf39b7bb2ae056042c20796
Akylbek Askharov <125436332+AskharovA@users.noreply.github.com>
2024-10-06 09:50:50 +0500

    Task-19 Add test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,6 +25,19 @@ async def setup_database(check_test_mode):
 
 
 @pytest.fixture(scope="session", autouse=True)
+async def add_data(setup_database):
+    with open("tests/mock_hotels.json", encoding="utf-8") as hotels_json:
+        hotels_data = [HotelAdd(**hotel) for hotel in json.load(hotels_json)]
+    with open("tests/mock_rooms.json", encoding="utf-8") as rooms_json:
+        rooms_data = [RoomAdd(**room) for room in json.load(rooms_json)]
+
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        await db.hotels.add_bulk(hotels_data)
+        await db.rooms.add_bulk(rooms_data)
+        await db.commit()
+
+
+@pytest.fixture(scope="session", autouse=True)
 async def register_user(setup_database):
     async with AsyncClient(app=app, base_url="http://test") as ac:
         await ac.post(
