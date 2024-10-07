cf24891a2a038c2b3a05a045007541f4be636f27
Akylbek Askharov <125436332+AskharovA@users.noreply.github.com>
2024-10-07 14:58:27 +0500

    Task-25 Test auth

---
+++ tests/integration_tests/auth/test_api.py
@@ -0,0 +1,35 @@
+import pytest
+
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ("user1@gmail.com", "12345678", 200),
+    ("user2@gmail.com", "65756755", 200),
+    ("user1@gmail.com", "53453465", 402),
+    ("user3#gmail.com", "88768767", 422),
+    ("user4@gmail.com", "88768767", 200),
+])
+async def test_auth(email, password, status_code, ac):
+    user_data = {"email": email, "password": password}
+
+    response = await ac.post(
+        "/auth/register",
+        json=user_data
+    )
+    assert response.status_code == status_code
+
+    if response.json().get("status", None) == "OK":
+        await ac.post(
+            "/auth/login",
+            json=user_data
+        )
+        assert ac.cookies["access_token"]
+
+        response = await ac.get("/auth/me")
+        assert response.json()["email"] == email
+
+        response = await ac.delete("/auth/logout")
+        assert response.json()["status"] == "OK"
+        assert ac.cookies.get("access_token", None) is None
+
+        response = await ac.get("/auth/me")
+        assert response.status_code == 401
